0c2389ea79af65d2da478c0d859e7dee5c8d69ed
Leo Solar <adm@cptsoftware.com>
2021-10-02 17:53:26 -0300

    add npm bootstrap

---
+++ src/pages/Main/Login/Login.jsx
@@ -0,0 +1,19 @@
+import Login from '../../../components/Login/Login';
+import { BrowserRouter } from 'react-router-dom'
+
+function index() {
+  return (
+    
+      <div >
+      <body style={{ background: 'white',  }} className="hold-transition  login-page ">
+      <Login />
+      </body>
+    </div>
+   
+    
+   
+    
+  );
+}
+
+export default index;
