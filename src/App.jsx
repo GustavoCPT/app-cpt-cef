64711617b72be3b03bfff7ca3da12b50c8f6e804
Leo Solar <adm@cptsoftware.com>
2021-10-02 17:32:10 -0300

    add routes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Routes from './Routes/Routes';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div class="wrapper">
+      
+      <Routes />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
