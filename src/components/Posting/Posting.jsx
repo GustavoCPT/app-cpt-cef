9700a65b388b6afe1420df523dd4f7e954226254
Leo Solar <adm@cptsoftware.com>
2021-10-03 19:14:09 -0300

    ajuste2.6  tab

--- src/components/Posting/Posting.jsx
+++ src/components/Posting/Posting.jsx
@@ -7,116 +7,96 @@ import ListDataCefSummary from '../List/ListDataCefSummary';
 function Posting() {
   return (
 
-    <div className="content m-0 p-0 ">
-      <div className="container-fluid ml-2">
+    <div className="content  ">
+      <div className="container-fluid ">
 
-      <div className="ui padded grid" id="controle-planilha">
-        <div className="row">
-          <div className="column">
-            <div className="clearfix">
-              <div className="ui small buttons icon compact align-left">
-               
-                <div className="ui  right floated dropdown compact blue icon button">
-                  
-                  <span className="text">Ajuda</span>
-                  <div className="menu">
-                    <div data-value="ajuda_title_plano_conta_alterar" className="item"><i className="checkmark box icon" />  ${'{'}moduloPlanoConta.ajuda_title_plano_conta_alterar{'}'}</div>
-                    <div data-value="ajuda_title_lancamento_realizar" className="item"><i className="checkmark box icon" />  ${'{'}moduloPlanoConta.ajuda_title_lancamento_realizar{'}'}</div>
-                    <div data-value="ajuda_title_gerar_relatorios" className="item"><i className="checkmark box icon" /> ${'{'}moduloPlanoConta.ajuda_title_gerar_relatorios{'}'} </div>
-                    <div data-value="ajuda_title_outras_informacoes" className="item"><i className="checkmark box icon" /> ${'{'}moduloPlanoConta.ajuda_title_outras_informacoes{'}'} </div>
+        <div className="ui padded grid " id="controle-planilha">
+          <div className="row pl-2">
+            <div className="column">
+              <div className="clearfix">
+                <div className="ui small buttons icon compact align-left">
+
+                  <div className="ui  right floated dropdown compact blue icon button">
+
+                    <span className="text">Ajuda</span>
+                    
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="ui segment " id="ui_data_ano_lancamentos">
-              <div className="ui grid">
-                <div className="eight wide column">
-                  <div className="ui medium form ">
-                    <div className="inline field" style={{ marginBottom: 0 }}>
-                     
-                      <input id="ano_lancamento" placeholder className="somenteNumeros limitaNumCaracteres4" type="text" size={4} />
-                      <a className="ui blue tag label">ANO</a>
+              <div className="ui segment " id="ui_data_ano_lancamentos">
+                <div className="ui grid">
+                  <div className="eight wide column">
+                    <div className="ui medium form ">
+                      <div className="inline field" style={{ marginBottom: 0 }}>
+
+                        <input id="ano_lancamento" placeholder className="somenteNumeros limitaNumCaracteres4" type="text" size={4} />
+                        <a className="ui blue tag label">ANO</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="eight wide column">
+                    <div className="ui  block align-right">
+                      <button className="ui positive icon button btnreceita p-2 ">
+
+                        RECEITAS
+                      </button>
+                      <button className="ui negative   icon button btndespesa p-2 ">
+
+                        DESPESAS
+                      </button>
+                      <button className="ui purple   icon button btninvestimento p-2 ">
+
+                        INVESTIMENTOS
+                      </button>
                     </div>
                   </div>
                 </div>
-                <div className="eight wide column">
-                  <div className="ui  block align-right">
-                    <button  className="ui positive icon button btnreceita">
-                      
-                      RECEITAS
-                    </button>
-                    <button  className="ui negative   icon button btndespesa">
-                      
-                      DESPESAS
-                    </button>
-                    <button  className="ui purple   icon button btninvestimento">
-                     
-                      INVESTIMENTOS
-                    </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className="content mt-3">
+          <div className="container-fluid">
+            <ListDataCefSummary />
+
+          </div>
+        </div>
+
+
+        <div className="content mt-3">
+          <div className="container-fluid">
+            <ListDataCef />
+
+          </div>
+        </div>
+
+
+
+
+        <div className="ui padded grid" id="ui_grafico">
+          <div className="row pl-3">
+            <div className="column">
+              <div className="ui segment">
+                <div className="ui grid">
+                  <div className="sixteen wide column">
+                    <h2 className="ui horizontal color-header-text">
+
+                    </h2>
+                    <GraphCef />
+
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
+
+
       </div>
-
-      <div className="ui padded grid" id="ui_tabela_resumo">
-        <div className="row">
-          <div className="column">
-            <div className="ui segment">
-              <div className="ui grid">
-                <div className="sixteen wide column">
-                  
-                   <ListDataCefSummary />
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="ui padded grid" id="ui_tabela_lancamentos">
-        <div className="row">
-          <div className="column">
-            <div className="ui segment">
-              <div className="ui grid">
-                <div className="sixteen wide column">
-                  
-                    <ListDataCef />
-                 
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      
-
-      <div className="ui padded grid" id="ui_grafico">
-        <div className="row">
-          <div className="column">
-            <div className="ui segment">
-              <div className="ui grid">
-                <div className="sixteen wide column">
-                  <h2 className="ui horizontal color-header-text">
-                    
-                  </h2>
-                  <GraphCef />
-                  
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-    </div>
     </div>
 
 
